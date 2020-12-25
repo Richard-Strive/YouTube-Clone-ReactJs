3bfbf547032388ac5e98bd192bdd11dd35a6864e
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2020-12-25 20:56:10 +0100

    Okay
    Sono produttivo.
    
    Ci volevano proprio queste vacanze per fare mente locale.
    
    Completare il progetto entro oggi...

--- my-youtube-clone/src/components/SearchBar.jsx
+++ my-youtube-clone/src/components/SearchBar.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { Paper, TextField } from "@material-ui/core";
 
 class SearchBar extends React.Component {
   state = {
     searchTerm: "",
   };
+
+  handleChange = (e) => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
   render() {
-    return <h1>I am a search bar</h1>;
+    return (
+      <Paper elevation={6} style={{ padding: "20px" }}>
+        <form>
+          <TextField
+            fullWidth
+            label="Search..."
+            onChange={(e) => this.handleChange(e)}
+          ></TextField>
+        </form>
+      </Paper>
+    );
   }
 }
 
